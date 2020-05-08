cdb5582306ba85715d8aab638f8ba48f9941c904
Qiaosen Huang <joesonw@gmail.com>
2020-05-08 23:16:20 +0800

    fix application.properties missing

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 
 export VERSION=$(xq .project.version pom.xml | tr -d '"')
 echo "Building version: ${VERSION}"
+cat << EOF |> src/main/resources/application.yaml
+server.port: 8080
+kafka.servers: []
+EOF
 mvn clean package spring-boot:repackage
 cp target/kafka-admin-api-${VERSION}.jar /app.jar
