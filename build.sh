ddf2a525b06b6b10d6ccdaa8cc4f73cfe427ab45
Qiaosen Huang <joesonw@gmail.com>
2020-05-09 10:32:51 +0800

    fix build

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 export VERSION=$(xq .project.version pom.xml | tr -d '"')
 echo "Building version: ${VERSION}"
 mkdir -p src/main/resources
-cat << EOF |> src/main/resources/application.yml
-server.port: 8080
-kafka.servers: []
+cat << EOF |> src/main/resources/application.properties
+server.port = 8080
+kafka.servers: localhost:9092
 EOF
 mvn clean package spring-boot:repackage
 cp target/kafka-admin-api-${VERSION}.jar /app.jar
