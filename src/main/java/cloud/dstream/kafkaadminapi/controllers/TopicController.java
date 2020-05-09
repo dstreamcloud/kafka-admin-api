0c0a141d89857cdb4f2419f4bb69e5d9fc8e1361
Qiaosen Huang <joesonw@gmail.com>
2020-05-09 18:00:51 +0800

    allow increase topic partitions

--- src/main/java/cloud/dstream/kafkaadminapi/controllers/TopicController.java
+++ src/main/java/cloud/dstream/kafkaadminapi/controllers/TopicController.java
@@ -21,9 +21,7 @@ public class TopicController {
 
     @GetMapping("/{name}")
     ResponseDTO get(@PathVariable("name") String name) throws Exception {
-        DescribeTopicsResult result = adminClient.describeTopics(Collections.singleton(name));
-        TopicDescription desc = result.all().get().get(name);
-        return new ResponseDTO(new TopicDTO(desc));
+        return new ResponseDTO(describeTopic(name));
     }
 
     @PostMapping("")
@@ -33,10 +31,29 @@ public class TopicController {
         return new ResponseDTO(new TopicDTO(body.getName()));
     }
 
+    @PutMapping("")
+    ResponseDTO update(@RequestBody NewTopicDTO body) throws Exception {
+        TopicDTO oldTopic = describeTopic(body.getName());
+
+        NewPartitions newPartitions;
+        newPartitions = NewPartitions.increaseTo(body.getNumPartitions());
+        Map<String, NewPartitions> createPartitions = new HashMap<>();
+        createPartitions.put(body.getName(), newPartitions);
+
+        adminClient.createPartitions(createPartitions).all().get();
+        return new ResponseDTO(describeTopic(body.getName()));
+    }
+
     @DeleteMapping("/{name}")
     ResponseDTO delete(@PathVariable("name") String name) throws Exception {
         DeleteTopicsResult result = adminClient.deleteTopics(Collections.singleton(name));
         result.all().get();
         return new ResponseDTO(new TopicDTO(name));
+    }
+
+    private TopicDTO describeTopic(String name) throws Exception {
+        DescribeTopicsResult result = adminClient.describeTopics(Collections.singleton(name));
+        TopicDescription desc = result.all().get().get(name);
+        return new TopicDTO(desc);
     }
 }
